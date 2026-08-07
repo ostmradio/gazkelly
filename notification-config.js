0454bc3edbf58d5190ad15447409a7425a503010
Alek <admin@ostm.co.uk>
2026-08-07 15:35:13 +0100

    Update notification-config.js
    * Updated the SSDHHE notification from "Limited" to "Buy now!"

--- notification-config.js
+++ notification-config.js
@@ -2,9 +2,9 @@
 // Change enabled to false when you do not want the notification to appear.
 window.siteNotificationConfig = {
     enabled: true,
-    id: "limited-SSDHHE-vinyl-booklet",
-    label: "Limited",
-    message: 'Buy Limited Edition SSDHHE 12" Vinyl + Booklet',
+    id: "SSDHHE-vinyl-booklet",
+    label: "BUY NOW!",
+    message: 'Buy the SSDHHE 12" Vinyl + Booklet now!',
     url: "https://elasticstage.com/gazkelly555whiteversion/releases/some-stories-dont-have-happy-endings-album",
     delay: 500
 };
